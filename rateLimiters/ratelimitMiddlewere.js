96a1d841e0d339f04288c7687c1ddc902f1dd455
Shubham <sanglehsubham9@gmail.com>
2023-02-15 23:47:46 +0530

    Rate limit Implemeted

---
+++ rateLimiters/ratelimitMiddlewere.js
@@ -0,0 +1,37 @@
+import Redis from 'ioredis'
+
+
+// how to create redis middleware using nodeJS
+let redis = new Redis()
+
+export async function rateLimit(req,res,next)
+{
+
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    
+    const requstCount = await redis.incr(ip)
+
+    //apply cooldown
+
+    if(requstCount == 1)
+    {
+        redis.expire(ip, 60)
+    }
+    else{
+        console.log('time to live : ',await redis.ttl(ip))
+    }
+
+    // cooldown emits ttl which is time to live ... how much time we have fot this
+
+    if(requstCount > 20)
+    {
+        res.send("Rate limiting").status(403)
+    }
+    else{
+        
+        console.log(`Request Made so far ${requstCount}`)
+        next()
+        // res.send("Success")
+    }
+
+}
--- router/router.js
+++ router/router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { isSiteUp, enumerateCountry } from "../controller/controller.js";
+import { rateLimit } from "../rateLimiters/ratelimitMiddlewere.js";
 
 const router = Router();
 
 router.get("/", isSiteUp);
-router.get("/enumerateCountry", enumerateCountry);
+router.get("/enumerateCountry", rateLimit ,enumerateCountry);
 
 export default router;
